62734fc12648fd4c679cf837e5c300c8c914475c
Hugo Moragues <hugo.moragues@gmail.com>
2018-01-22 16:15:55 +0100

    -

--- hello.cbl
+++ hello.cbl
@@ -1,6 +1,6 @@
 IDENTIFICATION DIVISION.
 PROGRAM-ID. HELLO-WORLD.
-*> simple hello world program
+*> simple hello world program demo
 PROCEDURE DIVISION.
     DISPLAY 'Hello world!'.
     STOP RUN.
